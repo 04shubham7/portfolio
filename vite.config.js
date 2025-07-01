e4bc4fd1b049faa02f56de2cae8a72de8e674450
Shubham Kumar <shubham040711@gmail.com>
2025-07-01 23:20:05 +0530

    npm buld

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist", // default, but make it explicit
+    chunkSizeWarningLimit: 1000, // optional: prevent chunk warning spam
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // optional: you can define chunk splitting if needed
+      },
     },
   },
 });
